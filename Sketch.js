518b783939a3ab9363c75d7349d1ae373c83773f
robinamila <robinamila66@gmail.com>
2021-01-19 01:05:44 +0100

    new game

--- Sketch.js
+++ Sketch.js
@@ -1,28 +1,57 @@
 var ship;
 var asteroids = [];
+var lasers = [];
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
     ship = new Ship();
-    for (var i = 0; i < 5; i++) {
+    for (var i = 0; i < 10; i++) {
     asteroids.push(new Asteroid());
 } 
 }
 
 function draw(){
     background(0);
-    ship.render();
-    ship.turn();
-    ship.update(); 
-    ship.edges();
+    
 
     for (var i = 0; i < asteroids.length; i++) {
+        if (ship.hits(asteroids[i])) {
+            console.log("ups");
+        }
         asteroids[i].render();
         asteroids[i].update();
         asteroids[i].edges();
 
     }
+
+    for (var i = lasers.length-1; i >=0; i--) {
+        lasers[i].render();
+        lasers[i].update();
+        if (lasers[i].offscreen()) {
+            lasers.splice(i, 1);
+        } else {
+        for (var j = asteroids.length-1; j >= 0; j--){
+        if (lasers[i].hits(asteroids[j])) {
+         if (asteroids[j].r > 10){
+         var newAsteroids = [j].breakup();
+         asteroids = asteroids.concat(newAsteroids);
+         } 
+         asteroids.splice(j, 1);
+         lasers.splice(i, 1);
+         break;
+        }
+    }
+    }
 }
+
+    console.log(lasers.length);
+
+        ship.render();
+        ship.turn();
+        ship.update(); 
+        ship.edges();
+}  
+
 
 function keyReleased (){
     ship.setRotation (0);
@@ -30,11 +59,13 @@ function keyReleased (){
  }
 
 function keyPressed () {
-    if (keyCode == 68) {
+    if (keyCode == 32){
+        lasers.push(new Laser(ship.pos, ship.heading));
+    }   else if (keyCode == 68) {
         ship.setRotation (0.1);
     }   else if (keyCode == 65) {
         ship.setRotation (-0.1); 
     }   else if (keyCode == 87) {
         ship.boosting(true); 
     }
-}
+  }
--- asteroid.js
+++ asteroid.js
@@ -1,12 +1,23 @@
 
-function Asteroid() {
-    this.pos = createVector(random(width), random (height))
+function Asteroid(pos, r) {
+    if (pos) {
+        this.pos = pos.copy();
+    } else {
+    this.pos = createVector(random(width), random (height));
+    }
+    if (r) {
+    this.r = r*0.5;
+    } else {
+        this.r = random(15,50);
+    }
+
+
+
     this.vel = p5.Vector.random2D();
-    this.r = random(25, 100);
     this.total = floor (random(5,15))
     this.offset = [];
     for (var i = 0; i < this.total; i++) {
-        this.offset [i] = random(-15, 15)
+        this.offset [i] = random(-this.r*0.5, this.r*0.5)
     }
 
     this.update = function () {
@@ -24,13 +35,20 @@ function Asteroid() {
     for (var i = 0; i < this.total; i++) {
         var angle = map(i, 0 , this.total, 0, TWO_PI); 
         var r = this.r + this.offset[i]; 
-        var x = r * cos(angle);
+        var x = r * cos(angle); 
         var y = r * sin(angle);
         vertex (x,y);
 
     }
     endShape(CLOSE);
     pop ();
+    }
+
+    this.breakup = function() {
+        var newA = [];
+        newA[0] = new Asteroid(this.pos, this.r);
+        newA[1] = new Asteroid(this.pos, this.r);
+        return newA;
     }
 
     this.edges = function() {
